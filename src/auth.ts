9839aded40bce1b755160f1beb5b03106ee97f02
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-06-15 17:15:10 +0600

    correction

--- src/auth.ts
+++ src/auth.ts
@@ -5,8 +5,7 @@ import { db } from './db'
 export const auth = betterAuth({
   database: kyselyAdapter(db),
 
-  
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.Backend_Url || 'http://localhost:5000',
 
   emailAndPassword: {
     enabled: true,
@@ -17,5 +16,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-  trustedOrigins: ['http://localhost:3000'],
+  trustedOrigins: [
+    process.env.Frontend_AUTH_URL || 'http://localhost:3000',
+  ],
 })
